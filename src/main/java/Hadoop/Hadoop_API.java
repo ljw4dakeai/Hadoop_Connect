7361da397b60547e7ed6eb40b165d032d122e56e
ljw4dakeai <2631247954@qq.com>
2021-10-20 21:26:18 +0800

    ok

--- src/main/java/Hadoop/Hadoop_API.java
+++ src/main/java/Hadoop/Hadoop_API.java
@@ -15,14 +15,16 @@ public class Hadoop_API {
         boolean loop =true;
         while (loop){
             System.out.println("------------------------------------------------");
-            System.out.println("       Mkdir     :创建文件夹");
+            System.out.println("      Mkdir      :创建文件夹");
             System.out.println("    Deletedir    :删除文件夹");
+//            System.out.println("     Listfile    :列出文件");
+//            System.out.println("     ListFile    :列出文件夹");
+            System.out.println("    ListFiles    :列出文件夹");
+            System.out.println("     Showfile    :查看文件内容");
             System.out.println("CopyFromLocalFile:上传文件");
             System.out.println(" CopyTOLocalFile :下载文件");
-            System.out.println("     Rename      :重命名文件文件");
-
+            System.out.println("     Rename      :重命名文件");
             System.out.println("      exit       :退出");
-
             System.out.println("-----------------------------------------------");
             System.out.print("请输入英文：");
             key = scanner.next();//接受一个字符
@@ -67,10 +69,40 @@ public class Hadoop_API {
                     }
                     break;
 
+//                case "Listfile":
+//                    try {
+//                        H.Listfile();
+//                    } catch (InterruptedException | URISyntaxException e) {
+//                        e.printStackTrace();
+//                    }
+//                    break;
+//
+//                case "ListFile":
+//                    try {
+//                        H.ListFile();
+//                    } catch (InterruptedException | URISyntaxException e) {
+//                        e.printStackTrace();
+//                    }
+//                    break;
+                case "ListFiles":
+                    try {
+                        H.ListFiles();
+                    } catch (InterruptedException | URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                    break;
 
                 case "exit":
                     scanner.close();
                     loop = false;
+                    break;
+
+                case "Showfile":
+                    try {
+                        H.Showfile();
+                    } catch (InterruptedException | URISyntaxException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 default:
                     break;
--- src/main/java/Hadoop/Hadoop_Connect.java
+++ src/main/java/Hadoop/Hadoop_Connect.java
@@ -1,16 +1,13 @@
 package Hadoop;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
 import java.util.Scanner;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.junit.Test;
 
 public class Hadoop_Connect {
@@ -32,10 +29,14 @@ public class Hadoop_Connect {
         // 3 创建
         String Filename ;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入你要创建的文件夹名称：例如：/user/zoujiahao1905140016/demo-three");
+        System.out.println("请输入你要创建的文件名称：例如：/user/zoujiahao1905140016/demo-three");
         Filename = scanner.next();
 
-        fs.mkdirs(new Path(Filename));
+        if (fs.exists(new Path(Filename))){
+            System.out.println("目录已经存在");
+        }else {
+            fs.mkdirs(new Path(Filename));
+        }
 
         // 4 关闭资源
         fs.close();
@@ -57,9 +58,14 @@ public class Hadoop_Connect {
         // 3 删除
         String Filename ;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("请输入你要删除的文件夹名称：例如：/user/zoujiahao1905140016/demo-three");
+        System.out.println("请输入你要删除的文件名称：例如：/user/zoujiahao1905140016/demo-three");
         Filename = scanner.next();
-        fs.delete(new Path(Filename));
+        if (!fs.exists(new Path(Filename))){
+            System.out.println("要删除的文件不存在");
+        }else {
+            fs.delete(new Path(Filename));
+        }
+
 
         // 4 关闭资源
         fs.close();
@@ -74,7 +80,6 @@ public class Hadoop_Connect {
         configuration.set("dfs.replication", "2");
 
         // 2 添加配置文件
-
         Properties properties = new Properties();
         properties.load(new FileInputStream("src/main/java/Hadoop/Hadoop.properties"));
         String URI =  properties.getProperty("URI");
@@ -82,7 +87,6 @@ public class Hadoop_Connect {
         FileSystem fs = FileSystem.get(new URI(URI), configuration,user);
 
         // 3 上传文件
-
         String filename ;
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入你要上传的文件名称：例如：e:/banzhang.txt");
@@ -111,9 +115,10 @@ public class Hadoop_Connect {
         properties.load(new FileInputStream("src/main/java/Hadoop/Hadoop.properties"));
         String URI =  properties.getProperty("URI");
         String user = properties.getProperty("user");
+
         FileSystem fs = FileSystem.get(new URI(URI), configuration,user);
 
-        // 3 执行下载操作
+        // 3 下载文件
         String filename ;
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入你要下载的文件名称：例如：/user/zoujiahao1905140016/demo-three/banzhang.txt");
@@ -128,7 +133,11 @@ public class Hadoop_Connect {
         // Path src 指要下载的文件路径
         // Path dst 指将文件下载到的路径
         // boolean useRawLocalFileSystem 是否开启文件校验
-        fs.copyToLocalFile(false, new Path(filename), new Path(Filename), true);
+        if (!fs.exists(new Path(filename))){
+            System.out.println("要下载的文件不存在");
+        }else {
+            fs.copyToLocalFile(false, new Path(filename), new Path(Filename), true);
+        }
 
         // 4 关闭资源
         fs.close();
@@ -147,7 +156,7 @@ public class Hadoop_Connect {
         String user = properties.getProperty("user");
         FileSystem fs = FileSystem.get(new URI(URI), configuration,user);
 
-        // 3 修改文件名称
+        // 3 重命名文件
         String filename ;
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入原文件名称：例如：/user/zoujiahao1905140016/demo-three/banzhang.txt");
@@ -156,13 +165,153 @@ public class Hadoop_Connect {
         Scanner scanner2 = new Scanner(System.in);
         System.out.println("请输入修改后文件名称：例如：/user/zoujiahao1905140016/demo-three/bangzhan.txt");
         refilename = scanner2.next();
-        fs.rename(new Path(filename), new Path(refilename));
+
+        if (!fs.exists(new Path(filename))){
+            System.out.println("要重命名的文件不存在");
+        }else {
+            fs.rename(new Path(filename), new Path(refilename));
+        }
+
 
         // 4 关闭资源
         fs.close();
     }
 
+//    //文件列出
+//    public void Listfile() throws IOException, InterruptedException, URISyntaxException {
+//
+//        // 1 获取文件系统
+//        Configuration configuration = new Configuration();
+//
+//        // 2 添加配置文件
+//        Properties properties = new Properties();
+//        properties.load(new FileInputStream("src/main/java/Hadoop/Hadoop.properties"));
+//        String URI = properties.getProperty("URI");
+//        String user = properties.getProperty("user");
+//        FileSystem fs = FileSystem.get(new URI(URI), configuration, user);
+//
+//        // 3 列出文件
+//        String filename ;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("请输入要查看的文件夹：例如：/user/");
+//        filename = scanner.next();
+//
+//
+//        RemoteIterator<LocatedFileStatus> iterator = fs.listFiles(new Path(filename),true);
+//        while (iterator.hasNext()){
+//            LocatedFileStatus fileStatus = iterator.next();
+//            Path fullpath = fileStatus.getPath();
+//            System.out.println(fullpath);
+//        }
+//
+//        // 4 关闭资源
+//        fs.close();
+//
+//    }
+//
+//    public void ListFile() throws IOException, InterruptedException, URISyntaxException {
+//
+//        // 1 获取文件系统
+//        Configuration configuration = new Configuration();
+//
+//        // 2 添加配置文件
+//        Properties properties = new Properties();
+//        properties.load(new FileInputStream("src/main/java/Hadoop/Hadoop.properties"));
+//        String URI = properties.getProperty("URI");
+//        String user = properties.getProperty("user");
+//        FileSystem fs = FileSystem.get(new URI(URI), configuration, user);
+//
+//        // 3 列出文件和文件夹
+//        String Filename;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("请输入要查看的文件夹：例如：/user/");
+//        Filename = scanner.next();
+//
+//        FileStatus[] fileStatuses = fs.listStatus(new Path(Filename));
+//        for (FileStatus fileStatus : fileStatuses){
+//            if (fileStatus.isDirectory()){
+//                boolean isDir = fileStatus.isDirectory();
+//                String fullpath = fileStatus.getPath().toString();
+//                System.out.println("文件" + isDir + ",名称" + fullpath);
+//            }
+//        }
+//        fs.close();
+//
+//    }
 
+    //列出所有文件和目录
+    public void ListFiles() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+
+        // 2 添加配置文件
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src/main/java/Hadoop/Hadoop.properties"));
+        String URI = properties.getProperty("URI");
+        String user = properties.getProperty("user");
+        FileSystem fs = FileSystem.get(new URI(URI), configuration, user);
+
+        // 3 查看文件夹
+        String Filenames ;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入要查看的文件目录：例如：/user/");
+        Filenames = scanner.next();
+        if (!fs.exists(new Path(Filenames))){
+            System.out.println("要查看的文件目录不存在");
+        }else {
+            FileStatus[] fileStatuses = fs.listStatus(new Path(Filenames));
+            for (FileStatus fileStatus : fileStatuses){
+                System.out.println(fileStatus.getPath().toString());
+            }
+            RemoteIterator<LocatedFileStatus> iterator = fs.listFiles(new Path(Filenames),true);
+            while (iterator.hasNext()){
+                LocatedFileStatus fileStatus = iterator.next();
+                Path fullpath = fileStatus.getPath();
+                System.out.println(fullpath);
+            }
+        }
+
+        fs.close();
+    }
+
+    //查看文件内容
+
+    public void Showfile() throws IOException, InterruptedException, URISyntaxException {
+
+        // 1 获取文件系统
+        Configuration configuration = new Configuration();
+
+        // 2 添加配置文件
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src/main/java/Hadoop/Hadoop.properties"));
+        String URI = properties.getProperty("URI");
+        String user = properties.getProperty("user");
+        FileSystem fs = FileSystem.get(new URI(URI), configuration, user);
+
+        // 3 显示文件内容
+        String Filename;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入要查看的文件名：例如：/user/zoujiahao1905140016/demo-three/banzhang.txt");
+        Filename = scanner.next();
+        if (!fs.exists(new Path(Filename))){
+            System.out.println("要显示的文件不存在！");
+        }else{
+            FSDataInputStream  fsDataInputStream = fs.open(new Path(Filename));
+            InputStreamReader inputStreamReader = new InputStreamReader(fsDataInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            String string = bufferedReader.readLine();
+            while (string != null){
+                System.out.println(string);
+                string = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+            inputStreamReader.close();
+            fsDataInputStream.close();
+        }
+        fs.close();
+    }
 
 }
 
