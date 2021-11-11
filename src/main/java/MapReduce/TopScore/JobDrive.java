8ba14650bd4ed922c0704131f1a0b09fab983cde
ljw4dakeai <2631247954@qq.com>
2021-11-11 23:10:22 +0800

    ok

--- src/main/java/MapReduce/TopScore/JobDrive.java
+++ src/main/java/MapReduce/TopScore/JobDrive.java
@@ -1,6 +1,7 @@
 package MapReduce.TopScore;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -9,7 +10,10 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.Properties;
 
@@ -25,35 +29,37 @@ public class JobDrive {
         String user = properties.getProperty("user");
         FileSystem fs = FileSystem.get(new URI(URI), configuration,user);
 
-////        configuration.set("mapreduce.app-submission.cross-platform", String.valueOf(true));
-//        configuration.addResource("src/main/core-site.xml");
-//        configuration.addResource("src/main/hdfs-site.xml");
-//        configuration.addResource("src/main/mapred-site.xml");
-//        configuration.addResource("src/main/yarn-site.xml");
-//        configuration.addResource("src/main/hadoop-env.sh");
-//        configuration.set("fs.defaultFS", "hdfs://192.168.95.136:9000");
-//        configuration.set("mapred.job.tracker","192.168.95.136:9001");
-//        configuration.set("hadoop.job.user","root");
-//        configuration.set("mapreduce.framework.name", "yarn");
-//        configuration.set("yarn.resourcemanager.hostname", "node0");
-//        configuration.set("yarn.nodemanager.aux-services", "mapreduce_shuffle");
-//        configuration.set("hadoop.tmp.dir", "/data/hadoop/tmp");
+        //configuration配置，不会影响上面的fs配置
+        configuration.set("fs.defaultFS", "hdfs://192.168.95.136:9000");
+        configuration.set("mapred.job.tracker","192.168.95.136:9001");
+        configuration.set("hadoop.job.user","root");
+        configuration.set("mapreduce.framework.name", "yarn");
+        configuration.set("yarn.resourcemanager.hostname", "node0");
+        configuration.set("yarn.nodemanager.aux-services", "mapreduce_shuffle");
+        configuration.set("hadoop.tmp.dir", "/data/hadoop/tmp");
+        configuration.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
 
         //dir配置
-        String input = "hdfs://192.168.95.136:9000/user/zoujiahao1905140016/TopScore/input.txt" ;
-        String output = "hdfs://192.168.95.136:9000/user/zoujiahao1905140016/TopScore/output";
+        String input = "hdfs://192.168.95.136:9000/user/zoujiahao1905140016/MapReduce/TopScore/input.txt" ;
+        String output = "hdfs://192.168.95.136:9000/user/zoujiahao1905140016/MapReduce/TopScore/output";
         Path inputpath = new Path(input);
         Path outputpath = new Path(output);
+
+        //运行前删除outpath
+        fs.delete(new Path(output));
 
         //上传input，需要可以重新上传
         Path localinputpath = new Path("/Users/zoujiahao/IDEA/Hadoop_Connect/src/main/java/MapReduce/TopScore/localinput.txt");
         Path localoutputpath = new Path("/Users/zoujiahao/IDEA/Hadoop_Connect/src/main/java/MapReduce/TopScore/localoutput");
 
-//        fs.copyFromLocalFile(localinput, inputpath);
+        fs.copyFromLocalFile(localinputpath, inputpath);
+
+        String jar = "/Users/zoujiahao/IDEA/Hadoop_Connect/out/artifacts/Hadoop_Connect_MapReduce_TopScore_jar/Hadoop_Connect_MapReduce_TopScore.jar";
 
 
         //job配置
         Job job = Job.getInstance(configuration);
+        job.setJar(jar);
 
         //设置jar包
         job.setJarByClass(MapReduce.TopScore.JobDrive.class);
@@ -63,12 +69,12 @@ public class JobDrive {
         job.setMapOutputValueClass(IntWritable.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        job.setMapperClass(Map.class);
-        job.setReducerClass(Reduce.class);
+        job.setMapperClass(MapReduce.TopScore.Map.class);
+        job.setReducerClass(MapReduce.TopScore.Reduce.class);
 
         //输入输出配置配置
-        FileInputFormat.setInputPaths(job,localinputpath);
-        FileOutputFormat.setOutputPath(job,localoutputpath);
+        FileInputFormat.setInputPaths(job,inputpath);
+        FileOutputFormat.setOutputPath(job,outputpath);
         boolean result = job.waitForCompletion(true);
 
 
@@ -86,12 +92,26 @@ public class JobDrive {
 
         }
 
-        FileInputStream fileInputStream_output = new FileInputStream("/Users/zoujiahao/IDEA/Hadoop_Connect/src/main/java/MapReduce/TopScore/localoutput/part-r-00000");
-        System.out.println("output文件内容");
-        while ((size = fileInputStream_output.read(bytes)) != -1 ) {
-            content = new String(bytes, 0, size);
-            System.out.println(content);
+        FSDataInputStream fsDataInputStream = fs.open(new Path(output + "/part-r-00000"));
+        InputStreamReader inputStreamReader = new InputStreamReader(fsDataInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String string = bufferedReader.readLine();
+        while (string != null){
+            System.out.println(string);
+            string = bufferedReader.readLine();
         }
+        bufferedReader.close();
+        inputStreamReader.close();
+        fsDataInputStream.close();
+        fs.close();
+
+//        FileInputStream fileInputStream_output = new FileInputStream("/Users/zoujiahao/IDEA/Hadoop_Connect/src/main/java/MapReduce/TopScore/localoutput/part-r-00000");
+//        System.out.println("output文件内容");
+//        while ((size = fileInputStream_output.read(bytes)) != -1 ) {
+//            content = new String(bytes, 0, size);
+//            System.out.println(content);
+//        }
 
         System.exit(result ? 0 : 1);
 
--- src/main/java/MapReduce/WordCount/JobDrive.java
+++ src/main/java/MapReduce/WordCount/JobDrive.java
@@ -1,6 +1,7 @@
 package MapReduce.WordCount;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -10,7 +11,9 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.Properties;
 
@@ -28,51 +31,53 @@ public class JobDrive {
         String user = properties.getProperty("user");
         FileSystem fs = FileSystem.get(new URI(URI), configuration,user);
 
-////        configuration.set("mapreduce.app-submission.cross-platform", String.valueOf(true));
-////        System.setProperty("HADOOP_USER_NAME","zoujiahao");
-//        configuration.addResource("src/main/core-site.xml");
-//        configuration.addResource("src/main/hdfs-site.xml");
-//        configuration.addResource("src/main/mapred-site.xml");
-//        configuration.addResource("src/main/yarn-site.xml");
-//        configuration.addResource("src/main/hadoop-env.sh");
-//        configuration.set("fs.defaultFS", "hdfs://192.168.95.136:9000");
-//        configuration.set("mapred.job.tracker","192.168.95.136:9001");
-//        configuration.set("hadoop.job.user","root");
-//        configuration.set("mapreduce.framework.name", "yarn");
-//        configuration.set("yarn.resourcemanager.hostname", "node0");
-//        configuration.set("yarn.nodemanager.aux-services", "mapreduce_shuffle");
-//        configuration.set("hadoop.tmp.dir", "/data/hadoop/tmp");
+        //configuration配置，不会影响上面的fs配置
+        configuration.set("fs.defaultFS", "hdfs://192.168.95.136:9000");
+        configuration.set("mapred.job.tracker","192.168.95.136:9001");
+        configuration.set("hadoop.job.user","root");
+        configuration.set("mapreduce.framework.name", "yarn");
+        configuration.set("yarn.resourcemanager.hostname", "node0");
+        configuration.set("yarn.nodemanager.aux-services", "mapreduce_shuffle");
+        configuration.set("hadoop.tmp.dir", "/data/hadoop/tmp");
+        configuration.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
 
         //dir配置
-        String input = "hdfs://192.168.95.136:9000/user/zoujiahao1905140016/WordCount/input.txt" ;
-        String output = "hdfs://192.168.95.136:9000/user/zoujiahao1905140016/WordCount/output";
+        String input = "hdfs://192.168.95.136:9000/user/zoujiahao1905140016/MapReduce/WordCount/input.txt" ;
+        String output = "hdfs://192.168.95.136:9000/user/zoujiahao1905140016/MapReduce/WordCount/output";
         Path inputpath = new Path(input);
         Path outputpath = new Path(output);
+
+        //运行前删除outpath
+        fs.delete(new Path(output));
+
 
         //上传input，需要可以重新上传
         Path localinputpath = new Path("/Users/zoujiahao/IDEA/Hadoop_Connect/src/main/java/MapReduce/WordCount/localinput.txt");
         Path localoutputpath = new Path("/Users/zoujiahao/IDEA/Hadoop_Connect/src/main/java/MapReduce/WordCount/localoutput");
 
-//        fs.copyFromLocalFile(localinput, inputpath);
+        fs.copyFromLocalFile(localinputpath, inputpath);
+
+        String jar = "/Users/zoujiahao/IDEA/Hadoop_Connect/out/artifacts/Hadoop_Connect_MapReduce_WordCount_jar/Hadoop_Connect_MapReduce_WordCount.jar";
 
 
         //job配置
         Job job = Job.getInstance(configuration);
 
         //设置jar包
-        job.setJarByClass(JobDrive.class);
+        job.setJarByClass(MapReduce.WordCount.JobDrive.class);
+        job.setJar(jar);
 
 
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-        job.setMapperClass(Map.class);
-        job.setReducerClass(Reduce.class);
+        job.setMapperClass(MapReduce.WordCount.Map.class);
+        job.setReducerClass(MapReduce.WordCount.Reduce.class);
 
         //输入输出配置配置
-        FileInputFormat.setInputPaths(job,localinputpath);
-        FileOutputFormat.setOutputPath(job,localoutputpath);
+        FileInputFormat.setInputPaths(job,inputpath);
+        FileOutputFormat.setOutputPath(job,outputpath);
         boolean result = job.waitForCompletion(true);
 
 
@@ -90,7 +95,21 @@ public class JobDrive {
 
         }
 
-        System.exit(result ? 0 : 1);
+        System.out.println("输出文件内容");
+        FSDataInputStream fsDataInputStream = fs.open(new Path(output + "/part-r-00000"));
+        InputStreamReader inputStreamReader = new InputStreamReader(fsDataInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
+        String string = bufferedReader.readLine();
+        while (string != null){
+            System.out.println(string);
+            string = bufferedReader.readLine();
+        }
+        bufferedReader.close();
+        inputStreamReader.close();
+        fsDataInputStream.close();
+        fs.close();
+
+        System.exit(result ? 0 : 1);
     }
 }
